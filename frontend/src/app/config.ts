b32af902c0f0a16143e269e325e5582056cdc1cf
RaulUrdanetaG <raul.urdaneta2612@gmail.com>
2023-10-26 17:02:49 -0500

    migrated server to AWS ec2

--- frontend/src/app/config.ts
+++ frontend/src/app/config.ts
@@ -1,4 +1,5 @@
 export const AppConfig = {
-  baseUrl: 'https://chosu-inventory.onrender.com/api',
+  // baseUrl: 'https://chosu-inventory.onrender.com/api',
   // baseUrl: 'http://localhost:8080/api',
+  baseUrl: 'http://3.141.0.158/api',
 };
