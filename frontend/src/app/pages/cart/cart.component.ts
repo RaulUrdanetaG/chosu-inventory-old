0cee9269846ad39c14e3feb6017ffcb8faa1c187
RaulUrdanetaG <raul.urdaneta2612@gmail.com>
2023-10-29 17:57:29 -0500

    made cart page responsive

--- frontend/src/app/pages/cart/cart.component.ts
+++ frontend/src/app/pages/cart/cart.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Item } from 'src/app/interfaces/items';
 import { CartService } from 'src/app/services/cart.service';
 import { ItemsService } from 'src/app/services/items.service';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-cart',
@@ -12,11 +13,14 @@ export class CartComponent implements OnInit {
   cart: string[] = [];
   items: Item[] = [];
 
+  totalPrice: any;
+
   isLoadingItems: boolean = true;
 
   constructor(
     private cartService: CartService,
-    private itemsService: ItemsService
+    private itemsService: ItemsService,
+    public usersService: UsersService
   ) {}
 
   ngOnInit(): void {
@@ -29,13 +33,18 @@ export class CartComponent implements OnInit {
 
       // resolve all promises in the same order
       this.items = await Promise.all(promises);
+      this.totalPrice = this.items.reduce((price, item) => {
+        return price + item.price;
+      }, 0);
       this.isLoadingItems = false;
     });
   }
 
   async deleteItem(item: Item) {
+    // set default values to ensure loading screen appears
     this.isLoadingItems = true;
     this.items = [];
+
     this.cart = this.cart.filter((itemFilter) => itemFilter !== item._id);
     await this.cartService.updateCart(this.cart);
     this.cartService.getCart();
