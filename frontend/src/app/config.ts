5fa7bc7de3a31f81f8ec93dda9402641cad93a25
RaulUrdanetaG <raul.urdaneta2612@gmail.com>
2023-12-05 17:13:17 -0500

    fixed cart bugs

--- frontend/src/app/config.ts
+++ frontend/src/app/config.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  // baseUrl: 'https://chosu-inventory.onrender.com/api',
-  baseUrl: 'http://localhost:8080/api',
-  // whatsappBase:'https://wa.me/573002168494'
-  whatsappBase:'https://wa.me/573004519846'
+  baseUrl: 'https://chosu-inventory.onrender.com/api',
+  // baseUrl: 'http://localhost:8080/api',
+  whatsappBase:'https://wa.me/573002168494'
+  // whatsappBase:'https://wa.me/573004519846'
 };
